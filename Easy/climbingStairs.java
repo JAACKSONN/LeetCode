ecd5db576c7af81cf18c90abb80c3f3bd4472ae5
Jack Ellinger <jellinge@justjack.wifi.local.cmu.edu>
2022-02-27 12:40:57 -0500

    climbing stairs

--- Easy/climbingStairs.java
+++ Easy/climbingStairs.java
@@ -1,11 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int [] dp = new int [n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+        int [] prevArray = new int [n+1];
+        prevArray[0] = 1;
+        prevArray[1] = 1;
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i-2];
+            prevArray[i] = prevArray[i - 1] + prevArray[i-2];
         }
-        return dp[n];
+        return prevArray[n];
     }
 }
