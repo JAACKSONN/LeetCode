fdb2b483d8bb3c9b0aa2417acd62e9523a9aaa9c
Jack Ellinger <jackellinger@gmail.com>
2022-03-05 15:47:03 -0500

    isbalanced

--- Easy/isBalanced.java
+++ Easy/isBalanced.java
@@ -18,7 +18,6 @@ class Solution {
         if(root == null) return 0;
         return 1 + Math.max(height(root.left), height(root.right));
     }
-    
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
         int leftHeight = height(root.left);
