704090565e7d9eb901b30d6053ad895bd52565b0
Jack Ellinger <jackellinger@gmail.com>
2022-03-05 15:51:16 -0500

    lengthLast

--- Easy/lengthOfLastWord.py
+++ Easy/lengthOfLastWord.py
@@ -2,6 +2,5 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.strip()
         array = s.split(" ")
-        print(array)
         return len(array[len(array) - 1])
         
