0385d73ef496372cc4ac328be06e9097038fa0ca
Jack Ellinger <jackellinger@gmail.com>
2022-03-05 15:52:30 -0500

    ye

--- Easy/maxSubArray.py
+++ Easy/maxSubArray.py
@@ -4,6 +4,7 @@ class Solution:
         maxStore = []
         maxStore.append(tempMax)
         n = len(nums)
+        
         for i in range (1, n):
             maxStore.append(max(maxStore[i - 1] + nums[i], nums[i]))
             if maxStore[i] > tempMax:
