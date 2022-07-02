58f404f1b3b3142b9788126bfd19749f6de5497c
Jack Ellinger <jackellinger@gmail.com>
2022-07-02 00:08:32 -0400

    product except self

---
+++ Medium/productExceptSelf.py
@@ -0,0 +1,36 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [0] * len(nums)
+        postfix = [0] * len(nums)
+        
+#         Compute the prefix list
+
+        for i in range(len(nums)):
+            if i == 0:
+                prefix[0] = nums[0]
+            else:
+                prefix[i] = nums[i] * prefix[i - 1]
+#         Compute postfix list
+        for i in range(len(nums) - 1, -1, -1):
+            if i == len(nums) - 1:
+                postfix[len(nums) - 1] = nums[len(nums) - 1]
+            else:
+                postfix[i] = nums[i] * postfix[i + 1]
+        
+        res = [0] * len(nums)
+        
+        for i in range(len(nums)):
+            if i == 0:
+                prefixVal = 1
+            else:
+                prefixVal = prefix[i - 1]
+            if i == len(nums) - 1:
+                postfixVal = 1
+            else:
+                postfixVal = postfix[i + 1]
+            res[i] = prefixVal * postfixVal
+        
+        return res
+            
+        
+        
