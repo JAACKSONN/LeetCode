f62d9f131f4caa757033388f0d56d90dba036b00
Jack Ellinger <jackellinger@gmail.com>
2022-07-24 11:03:34 -0400

    subsets

---
+++ .history/Medium/subsets_20220724110318.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(i, curSubset):
+            if i >= len(nums):
+                res.append(curSubset.copy())
+                return
+            curSubset.append(nums[i])
+            backtrack(i + 1, curSubset)
+            curSubset.pop()
+            backtrack(i + 1, curSubset)
+        
+        backtrack(0, [])
+        return res
